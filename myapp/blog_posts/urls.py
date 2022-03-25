779a0471f301f7cd24922d61c5a081a01c2d4046
Sayan8981 <saayan8981@gmail.com>
2022-03-25 21:03:22 +0530

    #1 pagination added

--- myapp/blog_posts/urls.py
+++ myapp/blog_posts/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls import url
+from django.urls import re_path as url
 from blog_posts import views
 
 
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'blog_posts',
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
